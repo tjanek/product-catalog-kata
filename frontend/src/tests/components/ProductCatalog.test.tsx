8fae6b04cd78e5ec9f626acfbe8acc759e1efe84
Tomasz Janek <janek.tomasz@gmail.com>
2024-04-02 10:57:59 +0200

    get rid of vitest mocks

--- frontend/src/tests/components/ProductCatalog.test.tsx
+++ frontend/src/tests/components/ProductCatalog.test.tsx
@@ -1,27 +1,29 @@
-import {describe, expect, test, vi} from 'vitest';
+import {describe, expect, test} from 'vitest';
 import {render, screen} from '@testing-library/react';
 import {ProductCatalog} from "@/features/product-catalog";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-vi.mock("../api/api.product.ts", () => ({
-    getProducts: () => Promise.resolve([
-        {
-            id: "uuid-123",
-            name: "Product 1",
-            price: 19.99
-        }
-    ]),
-    deleteProduct: () => Promise.resolve(),
-    addProduct: () => Promise.resolve()
-}))
+const getProductCatalog = async () => Promise.resolve([
+    {
+        id: "uuid-123",
+        name: "Product 1",
+        price: 19.99
+    }
+])
+const deleteProductInCatalog = async () => Promise.resolve(null)
+const addProductToCatalog = async () => Promise.resolve(null)
 
 describe('ProductCatalog', () => {
     test('should render list of products', () => {
         render(
             <QueryClientProvider client={queryClient}>
-                <ProductCatalog/>
+                <ProductCatalog
+                    getProductsFn={getProductCatalog}
+                    addProductFn={addProductToCatalog}
+                    deleteProductFn={deleteProductInCatalog}
+                />
             </QueryClientProvider>
         );
 
